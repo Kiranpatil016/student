ab091ac99be6d58cdf08edfc5b9c8bbdbc5c0fc2
01fe22bca117 <01fe22bca117@kletech.ac.in>
2024-02-19 11:55:47 +0530

    code to print Student_details

---
+++ Stud_Course.java
@@ -0,0 +1,28 @@
+public class Stud_Course extends student {
+    void course()
+    {
+        String[] subjects = {"DevOps", "Machine Learning", "C#"};
+        int[][] marks = {
+            {50, 30, 60}
+           
+        };
+
+        // Assuming each row represents a student and each column represents a subject
+        for (int i = 0; i < marks.length; i++) {
+            for (int j = 0; j < marks[i].length; j++) {
+                if (marks[i][j] < 40) {
+                    System.out.println("Sameksha Bafna " + (i + 1) + " scored less than 40 in the  " + subjects[j]);
+                }
+            }    
+        }
+        }
+    public static void main(String args[])
+    {
+        Stud_Course sc = new Stud_Course();
+        sc.stud_det();
+        sc.course();
+       
+    }
+}
+
+
